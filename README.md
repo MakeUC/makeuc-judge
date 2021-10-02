7bad5db86bf2bd7c12748fae0171e93546b5ef2d
Dev Agrawal <devagrawal09@gmail.com>
2021-10-02 00:13:32 -0400

    revuc -> makeuc

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# RevolutionUC Admin
+# MakeUC Judging Portal
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 6.0.8.
 
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  BASE_URL: 'https://revolutionuc-api.herokuapp.com/api/v2/judging'
+  BASE_URL: 'http://ec2.makeuc.io:8082'
 };
